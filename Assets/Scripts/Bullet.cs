f8837966e3d6c85570829f938a52bd0682b974ce
khoido2003 <khoizpro31@gmail.com>
2025-08-22 23:43:10 +0700

    feat: improve bullet effect, upgrade map

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,9 @@ public class Bullet : MonoBehaviour
 {
     private Rigidbody rigidbody;
 
+    [SerializeField]
+    private GameObject bulletImpactVfx;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -13,6 +16,23 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactBulletFx(collision);
         Destroy(gameObject);
+    }
+
+    private void CreateImpactBulletFx(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            ContactPoint contact = collision.contacts[0];
+
+            GameObject newImpactFx = Instantiate(
+                bulletImpactVfx,
+                contact.point,
+                Quaternion.LookRotation(contact.normal)
+            );
+
+            Destroy(newImpactFx, 1f);
+        }
     }
 }
